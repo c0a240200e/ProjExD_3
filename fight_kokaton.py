e32bf592f8d8320171b709cec4c9408365c732d4
M.Yoda <c0a240200e@edu.teu.ac.jp>
2025-06-24 14:29:33 +0900

    練習2

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -174,12 +174,13 @@ def main():
                 if beam.rct.colliderect(bomb.rct):
                     beam = None
                     bomb = None
-        if bird.rct.colliderect(bomb.rct):
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
