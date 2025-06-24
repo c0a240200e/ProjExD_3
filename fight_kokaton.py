386557501a8820ddb6a332fcd2b66073cbb1311a
M.Yoda <c0a240200e@edu.teu.ac.jp>
2025-06-24 14:46:25 +0900

    練習4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,6 +165,9 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # 爆弾とこうかとんが衝突した場合
                 bird.change_img(8, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
